5e57a244e6dabecf79605698ed90a0174824ca23
Aaron Santana <santanavaldelomaraa@gmail.com>
2023-08-12 22:09:47 -0600

    database config

---
+++ backend/database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE rinconNorte;
+
+-- Path: backend/database.sql
+
+USE rinconNorte;
+
+-- Path: backend/database.sql
+
+CREATE TABLE cheeseProduction (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date DATETIME DEFAULT NOW(),
+    kg INT,
+    reposeStart DATE,
+    rennetStart DATE
+);
+
+
+
