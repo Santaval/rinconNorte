af8d694b905c5583156eff025ef1542b57624ca7
Aaron Santana <Aaron>
2023-12-14 19:57:06 -0600

    use new milk provider hook

---
+++ frontend/src/hooks/useNewMilkProvider.js
@@ -0,0 +1,31 @@
+import apiv1 from "../lib/apiv1";
+import { useState } from "react";
+import { toast } from "sonner";
+const useNewMilkProvider = () => {
+  const [newMilkProvider, setNewMilkProvider] = useState({
+    name: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handelChange = (e) => {
+    setNewMilkProvider({
+      ...newMilkProvider,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      await apiv1.post("/milkProvider", newMilkProvider);
+    } catch (err) {
+      toast.error(err.response.data);
+    }
+    setLoading(false);
+  };
+
+  return { newMilkProvider, handelChange, handelSubmit, loading };
+};
+
+export default useNewMilkProvider;
