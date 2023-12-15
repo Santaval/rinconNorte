c9bb3d4d8591b82330e697285ba3be6d9bea0ab2
Aaron Santana <Aaron>
2023-12-14 19:01:45 -0600

    product model

---
+++ backend/src/models/Product.js
@@ -0,0 +1,41 @@
+class ProductModel {
+
+    static async create({ name, price, materials, processTimes }) {
+        const id = codeGenerator(10);
+        const result = await pool.query("INSERT INTO product SET ?", {
+            id,
+            name,
+            price,
+            materials,
+            processTimes
+        });
+        if (result.affectedRows === 1) return { id, name, price, stock, category };
+        throw new Error("Error al crear producto");
+    }
+
+    static async edit({ id, name, price, materials, processTimes }) {
+        const result = await pool.query("UPDATE product SET ? WHERE id = ?", [
+            { name, price, materials, processTimes },
+            id,
+        ]);
+        if (result.affectedRows === 1) return { id, name, price, stock, category };
+        throw new Error("Error al editar producto");
+    }
+
+    static async delete({ id }) {
+        const result = await pool.query("DELETE FROM product WHERE id = ?", [id]);
+
+        if (result.affectedRows === 0) throw new Error("Error al eliminar producto");
+    }
+
+    static async getAll({ page }) {
+        const total = await pool.query("SELECT COUNT(*) FROM product");
+        const result = await pool.query("SELECT * FROM product LIMIT 10 OFFSET ?", [(page - 1) * 10]);
+        return {
+            total: total[0]["COUNT(*)"],
+            result,
+            page: page,
+            pages: Math.ceil(total[0]["COUNT(*)"] / 10),
+        };
+    }
+}
